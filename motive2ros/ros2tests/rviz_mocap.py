e166817bcee6359613648b69c9daded8c570777a
Ben <benfox0621@gmail.com>
2025-07-15 16:22:57 -0500

    deleted junk folders

--- motive2ros/ros2tests/rviz_mocap.py
+++ motive2ros/ros2tests/rviz_mocap.py
@@ -3,7 +3,7 @@ import time
 import rclpy
 def main(): 
     ids = {10,11}
-    MocapCaller("10.131.220.228","10.131.196.172", ids)
+    MocapCaller("10.133.218.144","10.131.196.172", ids)
 
 
 if __name__ == '__main__':
